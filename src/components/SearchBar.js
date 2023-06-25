6cc3fea18a4414f8772e00ebad9555c10c226929
Salau Tobiloba <103784167+Tobbiloba@users.noreply.github.com>
2023-06-25 23:39:16 +0100

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-// import './SearchBar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import CenterFocusWeakIcon from '@mui/icons-material/CenterFocusWeak';
 const SearchBar = ({ onSubmit }) => {
@@ -12,18 +11,16 @@ const SearchBar = ({ onSubmit }) => {
     const handleInputChange = (e) => {
         setTerm(e.target.value)
     }
-    // console.log(term)
     return (
-        <div className='mt-[1rem] lg:mt-0 lg:w-[70%]'>
-            <form onSubmit={handleFormSubmit} className='md:flex'>
-                <SearchIcon className='absolute mt-[.3rem] md:mt-[.75rem]' style={{ color: '#718096', width: '3rem', }} />
-                <input onChange={handleInputChange} value={term} placeholder='Search...' className='w-[100%] pl-[2.5rem] text-slate-600 py-[.3rem] rounded-sm h-fit md:h-[3rem] outline-none ' />
-                <button className='mt-[.75rem] md:mt-0  px-4 py-2 lg:py-0 bg-slate-500 cursor-pointer hover:bg-slate-800 w-[10rem]'><CenterFocusWeakIcon className='mr-2' />Generate</button>
+        <div className='lg:px-[10%] md:[3.5rem] px-[1.5rem] bg-slate-200 lg:mt-0 lg:w-[100%] lg:flex  py-[2rem] justify-between'>
+            <h1 className=' text-2xl font-mono font-bold lg:text-3xl'>Image <span className='text-slate-600'>Finder</span></h1>
+            <form onSubmit={handleFormSubmit} className='md:flex mt-[1.5rem] lg:justify-between lg:mt-0 lg:w-[70%]'>
+
+                <SearchIcon className='absolute mt-[.3rem] md:mt-[.75rem] ' style={{ color: '#718096', width: '3rem', }} />
+                <input onChange={handleInputChange} value={term} placeholder='Search...' className='rounded-xl w-[100%] pl-[2.5rem] text-slate-600 py-[.3rem] lg:w-[70%] h-fit md:h-[3rem] outline-none ' />
+                <button className='mt-[.75rem] rounded-xl md:mt-0  px-4 py-2 text-white font-mono lg:py-0 bg-slate-500 cursor-pointer hover:bg-slate-800 w-[10rem]'><CenterFocusWeakIcon className='mr-2' style={{ color: 'white' }} />Generate</button>
             </form>
 
-            {/* <div className='border'>
-
-            </div> */}
 
         </div>
     )
